9a9f7c5d0a357147f3e156a4dcbee13b55a2c44c
murat <muratulusal@yahoo.com>
2022-11-06 07:13:01 +0300

    runners created

--- src/test/java/com/bb2/runners/FailedTestRunner.java
+++ src/test/java/com/bb2/runners/FailedTestRunner.java
@@ -1,4 +1,15 @@
 package com.bb2.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/cydeo/step_definitions",
+        features = "@target/rerun.txt"
+)
+
+
 public class FailedTestRunner {
 }
